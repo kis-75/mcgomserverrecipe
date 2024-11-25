37a087efbf8482e6cd3241ccafd4a81309820337
kis-75 <kim19940705@gmail.com>
2024-11-26 03:02:05 +0900

    Update: recipes
    - 요리 레시피 업데이트

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="m-4">
+    <p class="text-2xl font-bold">곰서버 요리 레시피북입니다.</p>
+    <p>마지막 레시피 업데이트 일자: 2024-11-26 02:55:00</p>
+    <br>
+    <p class="text-xl">요리 레시피 관련 정보는 상단 탭의 '레시피 목록'에 있습니다.</p>
+    <br>
+    <p class="text-xl">'효율 계산' 탭은 계산에 사용되는 데이터가 부족하여 신뢰도가 낮습니다. 단순 참고용으로만 사용해주시기 바랍니다.</p>    
+</div>
